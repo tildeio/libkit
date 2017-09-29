ff4646379c894a529b5db2c42012e7c4733b2593
Godfrey Chan <godfreykfc@gmail.com>
2017-09-29 14:50:51 -0700

    Vendor ember-addon.js
    1. libkit is meant to be used as a "build time" dependency, so we
       cannot require libkit at "runtime".
    
    2. Using `__dirname` inside the extracted ember-addon.js will not
       do the right thing anyway.

--- files/ember-addon.js
+++ files/ember-addon.js
@@ -1,7 +1,19 @@
 'use strict';
 
-const { addon } = require('libkit');
+const path = require('path');
 
-module.exports = addon({
-  name: '<%= packageName %>'
-});
+module.exports = {
+  name: '<%= packageName %>',
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'self') {
+      this.treePaths.addon = path.resolve(__dirname, 'dist', 'modules', 'src');
+
+      registry.add('js', {
+        name: 'babel-with-app-settings',
+        ext: 'js',
+        toTree: tree => this.project.findAddonByName('ember-cli-babel').transpileTree(tree)
+      });
+    }
+  }
+};
